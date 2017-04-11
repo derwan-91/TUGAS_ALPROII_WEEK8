65affe0a7364a7f25759e56a3c80caf0994b21d8
Rizky Ramadhan <rizkyramadhanajjkh@gmail.com>
2017-04-11 12:13:37 +0700

    Update header.h

--- header.h
+++ header.h
@@ -1,35 +1,21 @@
-#include "header.h"
-int main()
-{
-    int IndexMenu;
-    do{
-        cout<<"                      Alpro II Task 1 Week 8                  "<<endl;
-        cout<<"                                By                            "<<endl;
-        cout<<"Anisa Latansa | Salima Zahra | Jovial Fachrul | Rizky Ramadhan"<<endl;
-        cout<<"Main Menu"<<endl;
-        cout<<"1.Insertion Sort\n2.Selection Sort\n3.Bubble Sort\n4.Quick Sort\n5.Exit"<<endl;
-        cout<<"choose one!"<<endl;
-        cin>>IndexMenu;
-        switch(IndexMenu){
-            case 1:
-                insertion();
-                    break;
-            case 2:
-                selection();
-                    break;
-            case 3:
-                bubble();
-                    break;
-            case 4:
-                quick();
-                    break;
-            case 5:
-                exit(0);
-                    break;
-            default :
-                cout<<"tidak ada dalam pilihan"<<endl;
-                    break;
-        }
-    }while(IndexMenu!=5);
-    return 0;
+#include <iostream> // standar header unutk c++
+#include <stdio.h> //standar header untuk c
+#include <cstdlib> //header untuk keluar program exit(0);
+using namespace std;
+
+//prosedur and function
+void insertion(){
+cout<<"welcome in insertion sort"<<endl;
+}
+
+void selection(){
+cout<<"welcome in selection sort"<<endl;
+}
+
+void bubble(){
+cout<<"welcome in bubble sort"<<endl;
+}
+
+void quick(){
+cout<<"welcome in quick sort"<<endl;
 }
